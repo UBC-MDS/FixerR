85cabcd36b4e1cacf982efbe7042d5676b772b4c
guanchenz <guanchen@Guanchens-MacBook-Pro.local>
2018-04-15 03:06:25 -0700

    add doc for data

---
+++ R/symbols-data.R
@@ -0,0 +1,15 @@
+#' Data for currency symbols
+#'
+#' @docType data
+#'
+#' @usage data(symbols)
+#'
+#' @format Data frame
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://fixer.io/documentation}{Fixer.io}
+#'
+#' @examples
+#' data(symbols)
+"symbols"
